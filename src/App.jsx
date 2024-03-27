df23285cdf756edb94720798288869a90b3799bf
Paul Scanlon <pauliescanlon@gmail.com>
2024-03-27 11:55:40 -0500

    refactor: abstract get-visits

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
 import LogVisit from './components/log-visit.jsx';
+import GetVisits from './components/get-visits.jsx';
 
 function App() {
-  const [visitors, setVisitors] = useState([]);
-
-  useEffect(() => {
-    const getVisits = async () => {
-      try {
-        const response = await fetch('/get-visits', {
-          method: 'GET',
-        });
-
-        const json = await response.json();
-        setVisitors(json.visits);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getVisits();
-  }, []);
-
   return (
     <>
       <h1>Log visit</h1>
       <LogVisit />
       <h2>Last 5 visits</h2>
-      <ol>
-        {visitors
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
-          .slice(0, 5)
-          .map((visit, index) => {
-            return (
-              <li key={index}>
-                <code>{JSON.stringify(visit, null, 2)}</code>
-              </li>
-            );
-          })}
-      </ol>
+      <GetVisits />
     </>
   );
 }
---
+++ src/components/get-visits.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+const GetVisits = () => {
+  const [visitors, setVisitors] = useState([]);
+
+  useEffect(() => {
+    const getVisits = async () => {
+      try {
+        const response = await fetch('/get-visits', {
+          method: 'GET',
+        });
+
+        const json = await response.json();
+        setVisitors(json.visits);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getVisits();
+  }, []);
+
+  return (
+    <ol>
+      {visitors
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .slice(0, 5)
+        .map((visit, index) => {
+          return (
+            <li key={index}>
+              <code>{JSON.stringify(visit, null, 2)}</code>
+            </li>
+          );
+        })}
+    </ol>
+  );
+};
+
+export default GetVisits;
