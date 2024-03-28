1470f16c18c206d350693a07819e73679007d285
Paul Scanlon <pauliescanlon@gmail.com>
2024-03-28 10:05:15 -0500

    refactor: add try catch to get-visits

--- netlify/edge-functions/get-visits.js
+++ netlify/edge-functions/get-visits.js
@@ -3,12 +3,19 @@ import { neon } from '@neondatabase/serverless';
 export default async function handler() {
   const sql = neon(Netlify.env.get('DATABASE_URL'));
 
-  const visits = await sql`SELECT * FROM visitors`;
+  try {
+    const visits = await sql`SELECT * FROM visitors`;
 
-  return new Response(JSON.stringify({ message: 'A-Ok!', visits }, null, 2), {
-    status: 200,
-    headers: { 'content-type': 'application/json' },
-  });
+    return new Response(JSON.stringify({ message: 'A-Ok!', visits }, null, 2), {
+      status: 200,
+      headers: { 'content-type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response({
+      status: 500,
+      headers: { 'content-type': 'application/json' },
+    });
+  }
 }
 
 export const config = { path: '/get-visits' };
